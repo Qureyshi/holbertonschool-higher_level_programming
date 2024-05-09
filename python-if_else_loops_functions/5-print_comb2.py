eb437e63f9689e5b62e02967a4b54d4c23c8bb6b
Qureyshi <you@example.com>
2024-05-09 15:49:19 +0000

    t4 t5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-for number in range(0,100):
-    if number < 10:
-        print(f"0{number},", end=" ")
-    elif number != 99:
-        print(f"{number},", end=" ")
-    elif number == 99:
-        print(f"{number}")
+for i in range(0, 100):
+    if i != 99:
+        print('{:02}'.format(i), end=', ')
+    else:
+        print('{:02}'.format(i))
