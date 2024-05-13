053b4e8bbafd4b447055095155f4cab0eefc90f4
Qureyshi <kamranqureyshi1998@gmail.com>
2024-05-13 16:42:13 +0000

    t2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    count = len(argv) - 1
+    print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, argv[i + 1]))
