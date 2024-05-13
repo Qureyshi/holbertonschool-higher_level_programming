ac5a78e13c8edf945267fbd16101c7f12868f9d7
Qureyshi <kamranqureyshi1998@gmail.com>
2024-05-13 16:47:59 +0000

    t2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,6 +4,11 @@ if __name__ == "__main__":
     from sys import argv
 
     count = len(argv) - 1
-    print("{} arguments:".format(count))
+    if length == 0:
+        print('0 arguments.')
+    elif length == 1:
+        print('1 argument:')
+    else:
+        print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, argv[i + 1]))
