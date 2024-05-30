aad2093f70791e92320e2c52c3b60150fd23215b
Qureyshi <kamranqureyshi1998@gmail.com>
2024-05-30 19:52:50 +0000

    t8910

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This is a "8-rectangle" module"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class based on BaseGeometry base class"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height 
