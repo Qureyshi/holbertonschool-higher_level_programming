68eacef8993fdd5772dcc54028952de6235dffe6
Qureyshi <kamranqureyshi1998@gmail.com>
2024-05-30 19:29:05 +0000

    t2345

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,6 +4,6 @@
 
 def inherits_from(obj, a_class):
     """Checks if an object is an inherited instance of a class"""
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and not type(obj) is a_class:
         return True
     return False
