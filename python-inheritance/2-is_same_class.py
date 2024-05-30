3d509b9a04f62a1e878607091a1bae580d7b7287
Qureyshi <kamranqureyshi1998@gmail.com>
2024-05-30 19:25:51 +0000

    t2345

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This is the "2-is_same_class" module.
+"""
+
+
+def is_same_class(obj, a_class):
+    """is same"""
+    return type(obj) is a_class
---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This is a '5-base_geometry' module"""
+
+
+class BaseGeometry:
+    """BaseGeometry class document string"""
+    pass
