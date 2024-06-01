e2a03d3d3b5923c57354c692acc5ce531634a32c
Qureyshi <kamranqureyshi1998@gmail.com>
2024-06-01 19:11:13 +0000

    t3

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,6 +7,6 @@ def text_indentation(text):
     for i in text:
         if i == "." or i == "?" or i == ":" :
             if i != text[-1]:
-                print(i)
+                print(i + "\n")
         else:
             print(i, end="")
