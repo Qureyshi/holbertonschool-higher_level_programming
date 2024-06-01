8eec4b9f41315bd81e245b223b770bcfe925b385
Qureyshi <kamranqureyshi1998@gmail.com>
2024-06-01 18:58:51 +0000

    t3

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""prints text"""
+
+
+def text_indentation(text):
+    """func"""
+    for i in text:
+    if i == "." or i == "?" or i == ":" :
+        print(i + "\n\n")
+    else:
+        print(i, end="")
