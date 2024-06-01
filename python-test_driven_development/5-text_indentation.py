230c6b9a23c05c75375e98c07f97949e2fd4acb1
Qureyshi <kamranqureyshi1998@gmail.com>
2024-06-01 19:41:39 +0000

    t3

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,16 +4,8 @@
 
 def text_indentation(text):
     """func"""
-    new_text = ""
-    new_text = text.replace(". ", ".")
-    new_text = new_text.replace(": ", ":")
-    new_text = new_text.replace("? ", "?")
-
-    for i in text:
-        if i == "." or i == "?" or i == ":" :
-            if i != text[-1]:
-                print(i)
-                print("\n\n")
-        else:
-            print(i, end="")
-    print("\n")
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for c in ".:?":
+        text = text.replace(c, c + "\n\n")
+    print("\n".join(s.strip() for s in text.split("\n")), end="")
