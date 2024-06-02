53c71b185ab17a053a60a42dc5b191cd49d06989
Qureyshi <kamranqureyshi1998@gmail.com>
2024-06-02 18:50:48 +0000

    inherit

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""This is a '5-base_geometry' module"""
+"""IS DOCUMENTED"""
 
 
 class BaseGeometry:
-    """BaseGeometry class document string"""
-
+    """IS DOCUMENTED"""
     def area(self):
-        """BaseGeometry class document string"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise  TypeError(f"{name} must be an integer")
+        self.name = name
+        self.value = value
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
