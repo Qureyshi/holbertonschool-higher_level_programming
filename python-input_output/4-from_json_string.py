067801de1e5e8c363723b7375a5d51c8044a4c17
Qureyshi <kamranqureyshi1998@gmail.com>
2024-06-04 21:50:21 +0000

    inputoutput

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This is a '3-to_json_string' module"""
+import json
+
+
+def from_json_string(my_str):
+    """returns the JSON representation of an object (string)"""
+
+    return json.loads(my_obj)
