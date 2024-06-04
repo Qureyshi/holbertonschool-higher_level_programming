24b8a6f8b98537ab4bc9afea8ec8efc0fe240db2
Qureyshi <kamranqureyshi1998@gmail.com>
2024-06-04 21:42:28 +0000

    inputoutput

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """This is '0-read_file' module"""
 
 
-def write_file(filename="", text=""):
+def append_file(filename="", text=""):
     """Opens file, reads it and prints to stdout"""
 
     with open(filename, "a", encoding="utf-8") as myfile:
